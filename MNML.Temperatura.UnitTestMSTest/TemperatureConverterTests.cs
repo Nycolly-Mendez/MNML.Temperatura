76025fc7fc65d91932931b0098dd2099fee0fd0a
Nycolly-Mendez <mistshellym@gmail.com>
2024-10-17 14:33:55 -0600

    proyecto de  temperatura

---
+++ MNML.Temperatura.UnitTestMSTest/TemperatureConverterTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MNML.ESFE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MNML.Temperatura.UnitTestMSTest
+{
+    [TestClass]
+    public class TemperatureConverterTests
+    {
+        [TestMethod]
+        public void CelsiusToFahrenheit_ShouldConvertCorrectly()
+        {
+            // Arrange
+            var converter = new TemperatureConverter();
+            double celsius = 25;
+
+            // Act
+            double result = converter.CelsiusToFahrenheit(celsius);
+
+            // Assert
+            Assert.AreEqual(77, result);
+        }
+
+        [TestMethod]
+        public void FahrenheitToCelsius_ShouldConvertCorrectly()
+        {
+            // Arrange
+            var converter = new TemperatureConverter();
+            double fahrenheit = 77;
+
+            // Act
+            double result = converter.FahrenheitToCelsius(fahrenheit);
+
+            // Assert
+            Assert.AreEqual(25, result, 0.001);
+        }
+    }
+}
---
+++ MNML.Temperatura.UnitTestxUnit/StringManipulatorTest.cs
@@ -0,0 +1,48 @@
+﻿using MNML.ESFE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MNML.Temperatura.UnitTestxUnit
+{
+    public class StringManipulatorTest
+    {
+        private StringManipulator _manipulator;
+
+        public StringManipulatorTest()
+        {
+            _manipulator = new StringManipulator();
+        }
+
+        [Fact]
+        public void ReverseString_ShouldReturnReversedString()
+        {
+            // Arrange
+            string input = "hello";
+            string expected = "olleh";
+
+            // Act
+            string result = _manipulator.ReverseString(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void RemoveSpaces_ShouldReturnStringWithoutSpaces()
+        {
+            // Arrange
+            string input = "hello world";
+            string expected = "helloworld";
+
+            // Act
+            string result = _manipulator.RemoveSpaces(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
